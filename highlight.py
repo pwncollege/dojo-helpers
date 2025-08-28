913f062537408ba79f120fe8d49f87ea9bf88285
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2025-08-28 11:35:51 +0300

    Add option to change the wrapper and add paths to ignore
    Added an option to change the wrapper from the default "`" to anything else, added an option to add paths that find_files_to_look_in won't search in.

--- highlight.py
+++ highlight.py
@@ -1,12 +1,16 @@
 import os
 from collections import deque
 
+# Change wrapper
+wrapper = "`"
+# Add paths to ignore
+paths_to_ignore = []
 # Add files to look in
-filesToLookIn = ["DESCRIPTION.md"]
+files_to_look_in = ["DESCRIPTION.md"]
 # Add words to highlight
-wordsToHighlight = ["root", "mv", "rm"]
+words_to_highlight = ["root", "mv", "rm"]
 
-def highlightWords(file, wordsToHighlight):
+def highlightWords(file, words_to_highlight, wrapper):
     with open(file, "r") as f:
         contents = f.read()
 
@@ -17,30 +21,33 @@ def highlightWords(file, wordsToHighlight):
             current_word += ch
 
         if not ch.isalpha():
-            if current_word.lower() in wordsToHighlight:
-                if ch != "`":
-                    current_word = '`' + current_word.lower() + '`'
+            if current_word.lower() in words_to_highlight:
+                if contents[i:i + len(wrapper)] != wrapper:
+                    current_word = wrapper + current_word.lower() + wrapper
 
             result.append(current_word)
             result.append(ch)
 
             current_word = ""
 
-
     with open(file, "w") as f:
         result.pop()
         f.write("".join(result))
 
 
+def find_files_to_look_in():
+    queue = deque(os.listdir())
 
-queue = deque(os.listdir())
+    while len(queue):
+        file = queue.pop()
+        if os.path.abspath(file) in paths_to_ignore:
+            continue
 
-while len(queue) > 0:
-    file = queue.pop()
-    if os.path.isdir(file):
-        for temp_file in os.listdir(file):
-            queue.append(os.path.join(file, temp_file))
+        if os.path.isdir(file):
+            for temp_file in os.listdir(file):
+                queue.append(os.path.join(file, temp_file))
 
-    elif os.path.basename(file) in filesToLookIn:
-        highlightWords(file, wordsToHighlight)
+        elif os.path.basename(file) in files_to_look_in:
+            highlightWords(file, words_to_highlight, wrapper)
 
+find_files_to_look_in()
