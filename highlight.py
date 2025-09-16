0f81b17a1ed0a0cbc59a841a027bd4b5c9c65f69
Leonabcd123 <156839416+Leonabcd123@users.noreply.github.com>
2025-09-16 21:12:09 +0300

    Improve performance and fixed bugs

--- highlight.py
+++ highlight.py
@@ -24,8 +24,11 @@ word_separators = [" ", "\n"]
 search_for_highlighted_words = True
 # Choose how many times a word has to highlighted before it gets added to words_to_highlight in get_highlighted_words
 # NOTE: only works when search_for_highlighted_words is True
-minimum_instances_of_highlighted_word = 4
+minimum_instances_of_highlighted_word = 1
 
+if search_for_highlighted_words:
+    word_count = {}
+    temp_words_to_highlight = set()
 
 def highlight_words(file):
     with open(file, "r") as f:
@@ -58,80 +61,79 @@ def highlight_words(file):
         result.pop()
         f.write("".join(result))
 
-def get_highlighted_words():
-    word_count = {}
-    temp_words_to_highlight = set()
-    for file in files:
-        with open(file, "r") as f:
-            contents = f.read()
+def get_highlighted_words(file):
+    with open(file, "r") as f:
+        contents = f.read()
 
-        wrapping = None
-        word_to_highlight = ""
-        i = 1
-        while i < len(contents) - 1:
-            if wrapping:
-                if contents[i - len(wrapping):i] == wrapping and not contents[i].isalpha():
-                    wrapping = None
-
-                i += 1
-                continue
-
-            if len(word_to_highlight) > 0:
-                if contents[i:i + len(wrapper)] == wrapper:
-                    if word_to_highlight in word_count:
-                        word_count[word_to_highlight] += 1
-                    else:
-                        word_count[word_to_highlight] = 1
-
-                    if word_count[word_to_highlight] >= minimum_instances_of_highlighted_word:
-                        temp_words_to_highlight.add(word_to_highlight)
-
-                    word_to_highlight = ""
-                    i += len(wrapper) + 1
-                    continue
-
-                word_to_highlight += contents[i]
-
-            if contents[i - len(wrapper):i] == wrapper:
-                if not wrapping:
-                    for wrapper_to_ignore, key in wrappers_to_ignore.items():
-                        if contents[i - 1:len(wrapper_to_ignore)] == wrapper_to_ignore:
-                            wrapping = key
-                            i += 1
-
-                    if wrapping:
-                        continue
-
-                word_to_highlight += contents[i]
+    contents += "0"
+    wrapping = None
+    word_to_highlight = ""
+    i = 1
+    while i < len(contents):
+        if wrapping:
+            if contents[i - len(wrapping):i] == wrapping and not contents[i].isalpha():
+                wrapping = None
 
             i += 1
+            continue
 
-    return list(temp_words_to_highlight)
+        if len(word_to_highlight) > 0:
+            if contents[i:i + len(wrapper)] == wrapper:
+                if word_to_highlight in word_count:
+                    word_count[word_to_highlight] += 1
+                else:
+                    word_count[word_to_highlight] = 1
 
+                if word_count[word_to_highlight] >= minimum_instances_of_highlighted_word:
+                    temp_words_to_highlight.add(word_to_highlight)
 
+                word_to_highlight = ""
+                i += len(wrapper) + 1
+                continue
+
+            word_to_highlight += contents[i]
+
+        if contents[i - len(wrapper):i] == wrapper:
+            if not wrapping:
+                for wrapper_to_ignore, key in wrappers_to_ignore.items():
+                    if contents[i - 1:len(wrapper_to_ignore)] == wrapper_to_ignore:
+                        wrapping = key
+
+                if wrapping:
+                    continue
+
+            word_to_highlight += contents[i]
+
+        i += 1
 
 
 def find_files_to_look_in():
     queue = deque(os.listdir())
+    files = []
 
     while len(queue):
-        file = queue.pop()
-        if os.path.abspath(file) in paths_to_ignore:
+        current_file = queue.pop()
+        if os.path.abspath(current_file) in paths_to_ignore:
             continue
 
-        if os.path.isdir(file):
-            for temp_file in os.listdir(file):
-                queue.append(os.path.join(file, temp_file))
+        if os.path.isdir(current_file):
+            for temp_file in os.listdir(current_file):
+                queue.append(os.path.join(current_file, temp_file))
 
-        elif os.path.basename(file) in files_to_look_in:
-            yield file
+        elif os.path.basename(current_file) in files_to_look_in:
+            if search_for_highlighted_words:
+                get_highlighted_words(current_file)
+                files.append(current_file)
+            else:
+                highlight_words(current_file)
+
+    return files
 
 
-files = list(find_files_to_look_in())
+files = find_files_to_look_in()
+
 
 if search_for_highlighted_words:
-    words_to_highlight = get_highlighted_words()
-
-
-for file in files:
-    highlight_words(file)
+    words_to_highlight = list(temp_words_to_highlight)
+    for file in files:
+        highlight_words(file)
